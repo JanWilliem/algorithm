c850d50a4cad9d9d0e2e9eb7d24df2fd3b1388f6
Haidong Wang <smithw1989@icloud.com>
2015-12-22 11:22:36 +0800

    777777

---
+++ Algorithm/src/com/nowcoder/sort/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.nowcoder.sort;
+
+public class SelectionSort {
+	public static void main(String[] args) {
+		int[] A = { 54, 35, 48, 36, 27, 12, 44, 44, 8, 14, 26, 17, 28 };
+		int n = 13;
+		int[] r = selectionSort(A, n);
+		System.err.println(r);
+	}
+
+	private static int[] selectionSort(int[] A, int n) {
+		int mini = 0;
+		for (int i = 0; i < n - 1; i++) {
+			mini = i;
+			for (int j = i + 1; j < n; j++) {
+				if (A[mini] > A[j])
+					mini = j;
+			}
+			int swap = A[mini];
+			A[mini] = A[i];
+			A[i] = swap;
+
+		}
+		return A;
+	}
+}
